259c7686e61f5d0293b60cad45e7c99db7a90aac
irgendsontyp <htcmaxx@gmail.com>
2018-01-15 21:50:22 +0100

    Removed requirements.txt, added dependency_links in setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
 	version = "1.0.0",
 	description = "Pipenv test app",
 	install_requires = ["pipenv-dependency"],
+	dependency_links = ["git+https://github.com/irgendsontyp/pipenv-dependency.git#egg=pipenv-dependency-1.0.0"],
 	packages = ["pipenvapp"]
 )
